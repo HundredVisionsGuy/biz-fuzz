c399e80362bb459d933bceb926c1b89a5ca1263d
Chris Winikka <3827026+HundredVisionsGuy@users.noreply.github.com>
2026-05-23 15:09:22 -0700

    changing main to prep for a qstacked layout

--- src/biz_fuzz/main.py
+++ src/biz_fuzz/main.py
@@ -1,7 +1,8 @@
 """
-basic_app.py
+main.py
 by HundredVisionsGuy
-A demo of the most basic input/output: labels, text inputs, and buttons.
+A TODO management app. A screen with a navbar and a Qstacked Layout for the various
+features (view list, create list, calendar view)
 """
 
 import sys
@@ -10,11 +11,12 @@ from PySide6.QtWidgets import (
     QApplication,
     QButtonGroup,
     QCheckBox,
+    QHBoxLayout,
     QLabel,
     QLineEdit,
-    QListWidget,
     QMainWindow,
     QPushButton,
+    QRadioButton,
     QVBoxLayout,
     QWidget,
 )
@@ -24,11 +26,20 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("To Do List")
+        self.setWindowTitle("Do It app")
         self.setContentsMargins(12, 12, 12, 12)
         self.resize(320, 240)
 
         layout = QVBoxLayout()
+        self.nav_group = QButtonGroup(self)
+        nav_widget = QWidget()
+        nav_layout = QHBoxLayout()
+        for option in ["home", "new", "my tasks", "calendar"]:
+            btn = QPushButton(option)
+            self.nav_group.addButton(btn)
+            nav_layout.addWidget(btn)
+        nav_widget.setLayout(nav_layout)
+
         title_label = QLabel("Create a Todo list by adding todo items.")
 
         # Widgets to create a Todo Item
@@ -36,10 +47,23 @@ class MainWindow(QMainWindow):
         self.name_input.setPlaceholderText("What do you want to do?")
 
         self.priority_label = QLabel("Set Priority Level ")
-        self.priority_list = QListWidget()
-        self.priority_list.addItems(["High", "Medium", "Low"])
-        self.priority_list.setFlow(QListWidget.LeftToRight)
-        self.priority_list.setCurrentRow(1)
+
+        # NOTE: a QButtonGroup is not a widget
+        self.priority_group = QButtonGroup()
+        self.priority_group.setExclusive(False)
+
+        # We have to set a widget for the QButtonGroup
+        priority_widget = QWidget()
+        priority_layout = QVBoxLayout()
+
+        # Add buttons to the group and the layout
+        for option in ["High", "Medium", "Low"]:
+            btn = QRadioButton(option)
+            self.priority_group.addButton(btn)
+            priority_layout.addWidget(btn)
+
+        # Add the layout to the widget
+        priority_widget.setLayout(priority_layout)
 
         add_todo_button = QPushButton("Add Item")
         add_todo_button.clicked.connect(self.add_todo_item)
@@ -56,12 +80,14 @@ class MainWindow(QMainWindow):
 
         # add widgets & layouts to main layout
         layout.addWidget(title_label)
+        layout.addWidget(nav_widget)
         layout.addWidget(self.name_input)
         layout.addWidget(self.priority_label)
-        layout.addWidget(self.priority_list)
+        layout.addWidget(priority_widget)
         layout.addWidget(add_todo_button)
         layout.addWidget(output_label)
         layout.addWidget(self.todo_container)
+
         # [OPTIONAL] Add a stretch to move everything up
         layout.addStretch()
 
@@ -75,8 +101,9 @@ class MainWindow(QMainWindow):
         todo_name = self.name_input.text()
         self.name_input.setText("")
 
-        todo_priority = self.priority_list.currentItem().text()
-        self.priority_list.clearSelection()
+        checked_priority = self.priority_group.checkedButton()
+        todo_priority = checked_priority.text()
+        checked_priority.setChecked(False)
 
         todo_string = f"{todo_name} -> {todo_priority}"
         todo_button = QCheckBox(todo_string)
