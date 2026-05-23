16bf50d7288581416c9d31268894e71be9e02fe0
Chris Winikka <3827026+HundredVisionsGuy@users.noreply.github.com>
2026-05-23 15:15:09 -0700

    Adding screen_widget base class

---
+++ src/biz_fuzz/screen_widget.py
@@ -0,0 +1,14 @@
+"""
+screen_widget.py
+by HundredVisionsGuy
+A base class for each screen in the stacked layout
+"""
+
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel
+
+
+class ScreenWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
